d158b45da72db37b95eecd23351a3a44350940fc
AlexisVS <alexis.vansan1440@gmail.com>
2023-05-30 09:08:34 +0200

    update

--- src/Models/modelRelation.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace AlexisVS\MultipassSecondTestingModule\Models;
-
-use App\Domain\Module\ModuleModel;
-
-class Model extends ModuleModel /** don't forget to extends App\Domain\Module\ModuleModel */
-{
-}
